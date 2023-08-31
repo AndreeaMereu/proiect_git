8d8a2f1ec42f891129aeb791083be51c28b8638c
AndreeaMereu <andreea.mereu13@gmail.com>
2023-08-31 21:07:46 +0300

    am adaugat un nou eveniment la apasarea butonului pentru afisarea numarului de km

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const myButton = document.getElementById("myButton");
 const textButon = document.getElementById("text");
-
+const KmCurmatura = document.getElementById("sumaKm");
 
 myButton.addEventListener("click", function() {
    textButon.textContent = "Buna, numele meu este AKI!"
+   KmCurmatura.textContent = "Km traseu Curmatura: " + km + " km";
 });
+
+const suma = (a,b) => a + b;
+const totalKm = (a, b, funct) => funct (a, b);
+const km = totalKm (10, 20, suma);
